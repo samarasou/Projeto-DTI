7fd24a29b674e20cd212b1f9d24be99cd8d5c53e
samarasou <samavic86@gmail.com>
2025-04-17 16:25:14 -0300

    Update sistema.js

--- Frontend/src/sistema.js
+++ Frontend/src/sistema.js
@@ -1,6 +1,7 @@
 // Bibliotecas
 
 import React, {useState} from "react";
+import ReactDOM from "react-dom/client";
 
 // Definição da Interface
 
@@ -11,28 +12,30 @@ const App = () => {
 const [alunos, setAlunos] = useState ([]);
 const [nome, setNome] = useState ("");
 const [nota, setNota] = useState (Array(5).fill(0));
-const [frequência, setFrequência] = useState ("");
+const [frequencia, setFrequencia] = useState ("");
 
 // Função Para Adionar Um Aluno
 
 const adicionarAluno = (e) => {
   e.preventDefault();
+
+  if (!nome || !frequencia || nota.some(n => n === 0)) {
+      alert ("Preencha os campos vazios.");
+      return;
+  }
+  
   const aluno = {
     nome,
     nota,
     frequencia: parseFloat(frequencia),
     media: nota.reduce((acc, nota) => acc + nota, 0) / nota.length
-    };
-    setAlunos([...alunos, aluno]);
-    setNome ("");
-    setNota(Array(5).fill(0));
-    setFrequência("");
+  };
+  
+  setAlunos([...alunos, aluno]);
+  setNome ("");
+  setNota(Array(5).fill(0));
+  setFrequência("");
 
-  } else {
-    if (!nome || !frequencia || nota.some(n => n === 0)) {
-      alert ("Preencha os campos vazios.");
-      return;
-  }
 };
 
 // Função Para Remover Um Aluno
@@ -64,7 +67,7 @@ return (
         <ul>
         {alunos.map((aluno, index) => (
           <li key= {index}>
-             {aluno.nome} | Nota: {aluno.notas.toFixed(2)} | Frequência: {aluno.frequencia}%
+             {aluno.nome} | Nota: {aluno.media.toFixed(2)} | Frequência: {aluno.frequencia}%
           </li>
         ))}
       </ul>
