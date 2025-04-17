0fcd39ef3f46f3c3b51261b7bb158bc82297039b
samarasou <samavic86@gmail.com>
2025-04-17 14:04:58 -0300

    Update sistema.js

--- Frontend/src/sistema.js
+++ Frontend/src/sistema.js
@@ -1,1 +1,23 @@
+// Bibliotecas
 
+import React, {useState} from "react";
+import ReactDom from "react-dom";
+
+// Definição da Interface
+
+const App = () => {
+
+// Definição dos Estados
+
+const [alunos, setAlunos] = useState ([]);
+const [nome, setNome] = useState ("");
+const [nota, setNota] = useState ("");
+
+// Função Para Adionar Um Aluno
+
+const adicionarAluno = (e) => {
+  e.preventDefault();
+  setAlunos([...alunos, { nome, nota }]);
+  setNome ("");
+  setNota("");
+};
